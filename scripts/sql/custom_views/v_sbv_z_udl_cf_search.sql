503fef7bdfe7e51d0a791341628f4fe96773bbe3
p08315 <emmanuel.gonon12@univ-lorraine.fr>
2026-04-21 12:02:25 +0200

    change view

--- scripts/sql/custom_views/v_sbv_z_udl_cf_search.sql
+++ scripts/sql/custom_views/v_sbv_z_udl_cf_search.sql
@@ -1,18 +1,13 @@
 DROP VIEW IF EXISTS splus.v_sbv_z_udl_cf_search;
 CREATE VIEW splus.v_sbv_z_udl_cf_search AS
     SELECT fikrs,
-        fictr,
-        beschr,
-        datbis,
-        datab
+           fictr,
+           beschr,
+           datbis,
+           datab
     FROM {target_schema}.fmfctr t1
-    WHERE (
-        (fikrs = '1010'::bpchar)
-        AND ((fictr >= '0000000000'::bpchar) AND (fictr <= 'ZZZZZZZZZZ'::bpchar))
-        AND ((datab IS NULL) OR (datab <= '20250304'::bpchar))
-        AND ((datbis IS NULL) OR (datbis >= '20250304'::bpchar)))
-    ORDER BY fictr
-    LIMIT 50;
+    WHERE fikrs = '1010'::bpchar
+    ORDER BY fictr;
 
 COMMENT ON COLUMN splus.v_sbv_z_udl_cf_search.fikrs IS 'Périmètre financier';
 COMMENT ON COLUMN splus.v_sbv_z_udl_cf_search.fictr IS 'Centre financier';
