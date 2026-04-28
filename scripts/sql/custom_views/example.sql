95feae7aa77ecccf469b7337f0ea390c04af81f0
p08315 <emmanuel.gonon12@univ-lorraine.fr>
2026-04-28 08:53:21 +0200

    correction documentation et vue example.sql

--- scripts/sql/custom_views/example.sql
+++ scripts/sql/custom_views/example.sql
@@ -1,5 +1,6 @@
 DROP VIEW IF EXISTS splus.reporting_csks;
 CREATE VIEW splus.reporting_csks AS
     SELECT c.bukrs, c.kostl, c.datbi, p.posid
-    FROM {target_schema}.csks AS c
-    CROSS JOIN {target_schema}.prps AS p;
+    FROM {target_schema}.csks c
+    JOIN {target_schema}.prps p ON c.bukrs = p.bukrs
+    WHERE c.datbi > CURRENT_DATE;
